221742b7d9a07df7bceb1b4a1c6b8c72c42dc633
faeiz-ff <faeizfaiza@gmail.com>
2026-06-11 23:52:26 +0700

    feat: add token

---
+++ src/token.zig
@@ -0,0 +1,91 @@
+const std = @import("std");
+
+pub const Token = enum {
+    // Non visible Token
+    Eof,
+    Unknown,
+
+    // single pass token
+    Asterisk,
+    Solidus,
+    Plus,
+    Modulus,
+    Ampersand,
+    Pipe,
+    Lbrace,
+    Rbrace,
+    Lparen,
+    Rparen,
+    Lsquare,
+    Rsquare,
+    Dot,
+    Comma,
+    Semicolon,
+    QuestionMark,
+    Colon,
+
+    // ambigous token, needs peek
+    Minus,
+    Less,
+    Greater,
+    Equal,
+    Bang,
+
+    // double char token
+    EqualEqual,
+    Arrow,
+    BangEqual,
+    LessEqual,
+    LessLess,
+    GreaterEqual,
+    GreaterGreater,
+
+    // literals
+    IntLiteral,
+    FloatLiteral,
+    StringLiteral,
+
+    // names
+    Identifier,
+
+    // keywords
+    Def,
+    Var,
+    Fun,
+    Use,
+    Pub,
+    As,
+
+    Nom,
+    Typ,
+    Tag,
+    Fit,
+    Own,
+
+    If,
+    Elif,
+    Else,
+    For,
+    In,
+    Match,
+    Case,
+    Break,
+    Continue,
+    Yield,
+    Return,
+
+    Not,
+    And,
+    Or,
+    False,
+    True,
+    Nil,
+
+    // primitives
+    Flt,
+    Bol,
+    Str,
+    Int,
+    Vec,
+    Tup,
+};
