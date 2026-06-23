bb49c0e8cb2e1ef922aa6b337c2360a517927ae7
faeiz-ff <faeizfaiza@gmail.com>
2026-06-23 17:29:43 +0700

    feat: remove VM stuff as it is not in development right now

--- src/root.zig
+++ src/root.zig
@@ -1,16 +1,11 @@
 const std = @import("std");
 const Io = std.Io;
 
-pub const Rle = @import("utils/rle.zig").Rle;
-pub const Value = @import("value.zig").Value;
-pub const Chunk = @import("chunk.zig").Chunk;
-pub const OpCode = @import("chunk.zig").OpCode;
-pub const VM = @import("vm.zig").VM;
-pub const debug = @import("debug.zig");
 pub const Token = @import("token.zig").Token;
 pub const TokenType = @import("token.zig").TokenType;
 pub const Lexer = @import("lexer.zig").Lexer;
 pub const LexerError = @import("lexer.zig").LexerError;
+pub const Errors = @import("errors.zig").Errors;
 
 test {
     std.testing.refAllDecls(@This());
